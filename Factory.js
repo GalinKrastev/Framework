001fcb7661405124fb6cbacc6d4dfe4c80d4f0ef
Galin <galinkrastev@gmail.com>
2015-11-09 07:49:45 +0200

    Initial commit

---
+++ Factory.js
@@ -0,0 +1,21 @@
+﻿/*
+* Factory function to create Controllers, Services ... etc.
+* @ type
+*/
+
+window.Milo = window.Milo || {};
+
+Milo.controllers = {};
+Milo.templates = {};
+
+
+function Factory() {
+    return {
+        Controller: Controller,
+        Service: Service
+    };
+
+    function Controller(name, dependencies) {
+
+    }
+};
---
+++ JsExtensions.js
@@ -0,0 +1,60 @@
+﻿Object.prototype.getType = Object.prototype.getType ||
+    function getType(propertyOrFunction, isOwnProperty) {
+        if (!propertyOrFunction && propertyOrFunction !== 0) {
+            throw new SyntaxError([
+                "Parameter propertyOrFunction is required ",
+                "and cannot be null, undefined or false"
+            ]);
+        }
+
+        if (this.type) {
+            return this.type;
+        }
+
+        if (
+            isOwnProperty === true &&
+            this.prototype[propertyOrFunction]
+        ) {
+            return;
+        }
+
+        return typeof this[propertyOrFunction];
+    };
+
+Object.prototype.extend = Object.prototype.extend ||
+    function extend(object) {
+        for (var propName in object) {
+            if (object.hasOwnProperty(propName)) {
+                if (object[propName][getType]) {
+                    this[propName] = {}.extend(object[propName]);
+                } else {
+                    this[propName] = object[propName];
+                }
+            }
+        }
+
+        if (object.prototype) {
+            this.prototype.extend(object.prototype);
+        }
+
+        return this;
+    };
+
+Object.prototype.tryToSetProperties = Object.prototype.tryToSetProperties ||
+    function tryToSetProperties(objWithProps) {
+        for (var prop in objWithProps) {
+            var addProps = true;
+            if (
+                objWithProps.hasOwnProperty(prop) &&
+                !this[prop] === undefined
+            ) {
+                addProps = false;
+            }
+        }
+
+        if (addProps) {
+            this.extend(objWithProps);
+        }
+
+        return this;
+    };
